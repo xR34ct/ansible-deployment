79e2cc1cceed82a7467efe734634abd5159a65f7
xR34ct <avdagic94@gmail.com>
2017-02-01 00:30:29 +0100

    Made changes to SSH

--- misc/kvm_clone_domain.sh
+++ misc/kvm_clone_domain.sh
@@ -5,9 +5,9 @@ echo "Cloning from $1 to $2"
 virt-clone --connect=qemu:///system -o "$1" -n "$2" --auto-clone
 virsh start "$2"
 echo "Started host $2..."
-hwaddress=$( virsh domiflist "$2" | grep "bridge" | awk '{ print $5 }' ) 
+hwaddress=$( virsh domiflist "$2" | grep "default" | awk '{ print $5 }' ) 
 while true; do
-	ipaddress=$(  arp-scan --interface=br0 -l | grep "$hwaddress" | awk '{ print $1 }' )
+	ipaddress=$(  arp-scan --interface=virbr0 -l | grep "$hwaddress" | awk '{ print $1 }' )
 	if [ -z "$ipaddress" ]; then     
 		echo "Scanning for ARP response..."
 	else
@@ -16,11 +16,15 @@ while true; do
 	fi
 done
 echo "Host $2 got MAC: $hwaddress and IP: $ipaddress"
-echo "$2	IN	A	$ipaddress" >> /etc/bind/zones/db.sciion.me
-service bind9 restart
+#echo "$2	IN	A	$ipaddress" >> /etc/bind/zones/db.sciion.me
+#service bind9 restart
 echo "$ipaddress	 hostname=$2" >> /etc/ansible/hosts
 echo "Running ansible provisioning..."
-su ansible -c "ansible-playbook ~/ansible/deploy_new_host.yml --limit=$ipaddress --ask-vault-pass"
+su ansible -c "ssh-keyscan -t rsa -H $ipaddress >> ~/.ssh/known_hosts"
+su xr34ct -c "ssh-keyscan -t rsa -H $ipaddress >> ~/.ssh/known_hosts"
+su ansible -c "ansible-playbook ~/ansible-deployment/deploy_new_host.yml --limit=$ipaddress --ask-vault-pass -u root --ask-pass --ask-sudo-pass"
+su xr34ct -c "ssh-keygen -R $ipaddress && ssh-keyscan -t rsa -H $ipaddress >> ~/.ssh/known_hosts"
+su ansible -c "ssh-keygen -R $ipaddress && ssh-keyscan -t rsa -H $ipaddress >> ~/.ssh/known_hosts"
 ping -c 1 "$2" > /dev/null
 echo "Deployment successfull!"
 exit 0
--- misc/kvm_destroy_domain.sh
+++ misc/kvm_destroy_domain.sh
@@ -2,9 +2,9 @@
 echo "Destroying host $1..."
 virsh destroy "$1"
 virsh undefine "$1"
-rm -f "/var/kvm/$1.qcow2"
+rm -f "/var/lib/libvirt/images/$1.qcow2"
 hosts=$( cat /etc/ansible/hosts | grep -v "$1" )
-dns=$( cat /etc/bind/zones/db.sciion.me | grep -v "$1" )
-echo "$dns" > /etc/bind/zones/db.sciion.me
+#dns=$( cat /etc/bind/zones/db.sciion.me | grep -v "$1" )
+#echo "$dns" > /etc/bind/zones/db.sciion.me
 echo "$hosts" > /etc/ansible/hosts
-service bind9 restart
+#service bind9 restart
